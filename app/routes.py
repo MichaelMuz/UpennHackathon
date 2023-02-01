bcef330cbc8fdbd850e3d83506d3723787370852
Michael Muzafarov <20muzm01@gmail.com>
2023-01-31 23:30:09 -0500

    basic form and questions

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import render_template, request
 
 @app.route("/")
 def index():
@@ -7,6 +8,21 @@ def index():
 @app.route("/my_orders")
 def crab_page():
     return "I don't know who is logged in yet"
+
+@app.route("/new_question/<user_id>", methods = ["GET"])
+def ask_question(user_id):
+    print(f"{user_id=}")
+    return render_template("new_question.html", user_id=user_id)
+
+@app.route("/new_question/<user_id>", methods = ["POST"])
+def enter_data(user_id):
+    question = request.form['question']
+    answer = request.form['answer']
+    print(f"{user_id=}, {question}, {answer}")
+    return render_template("question_recorded.html", user_id=user_id, question=question, answer=answer)
+
+    
+
 
 if __name__ == "__main__":
     app.run()

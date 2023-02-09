fe34a87def7ba365220accdd27c883b36e1ec1f7
MichaelMuz <20muzm01@gmail.com>
2023-02-09 20:18:15 +0000

    changed image size

--- backend/app/ai_layer.py
+++ backend/app/ai_layer.py
@@ -20,7 +20,7 @@ def dalle_req(prompt):
     response = openai.Image.create(
         prompt=prompt,
         n=1,
-        size="1024x1024"
+        size="256x256"
     )
     image_url = response['data'][0]['url']
     return image_url
